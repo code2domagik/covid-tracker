04ce1b581c306ae8618d8ae3a14b0470cb459213
Cristian <code2domagik@gmail.com>
2020-09-03 19:41:02 +0300

    adding new cases

--- src/main/java/io/rcr/covid19tracker/controllers/HomeController.java
+++ src/main/java/io/rcr/covid19tracker/controllers/HomeController.java
@@ -20,8 +20,11 @@ public class HomeController {
     public String home( Model model) {
         List<LocationStats> allStats = covidDataService.getStats();
         int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
 
         return "home";
     }
